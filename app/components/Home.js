b9c03d3fb764cb6edc8a90118fef777b4213accf
Amila Welihinda <amilajack@gmail.com>
2016-12-08 09:04:10 -0800

    iso-fetch-demo

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,8 +1,14 @@
 // @flow
 import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
 import { Link } from 'react-router';
 import styles from './Home.css';
 
+fetch('https://google.com')
+  .then(res => res.text())
+  .then(res => res.slice(0, 10))
+  .then(alert)
+  .catch(console.log)
 
 export default class Home extends Component {
   render() {
